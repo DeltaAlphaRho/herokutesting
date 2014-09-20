6f59c5187a9a15ba4d6459e2d4613088a097aee5
Delta Alpha Rho <Gentoo.T400@gmail.com>
2014-09-20 08:55:16 -0500

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/herokutesting-standalone.jar clojure.main -m herokutesting.web
---
+++ test/herokutesting/web_test.clj
@@ -0,0 +1,6 @@
+(ns herokutesting.web-test
+  (:require [clojure.test :refer :all]
+            [herokutesting.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
